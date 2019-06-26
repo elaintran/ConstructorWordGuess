23da11427d94935cfe479f283cd1be9f3e1749d2
elaintran <elain.ttran@gmail.com>
2019-06-26 16:18:07 -0500

    subtracts guesses if letter is not correct

--- Letter.js
+++ Letter.js
@@ -9,29 +9,23 @@ var randomWord = words[index];
 //start with an empty string
 //will print out undefined if left an empty variable
 var newWord = "";
+//total guesses
+var guess = 10;
+//letter found default to false
+var found;
 
 //function called every time the letters from the chosen word are looped through
 function Letter(letter) {
     //place letter value into object
     this.letter = letter;
-    //correct letter guessed is default to false
-    this.correctLetter = false;
     //piece together letters in a word
     this.wordDisplay = function() {
         //if guessed correctly, reveal letter
-        if (this.correctLetter === true) {
+        if (this.correct === true) {
             return this.letter;
         //if incorrect, display an underscore instead
         } else {
             return "_";
-        }
-    }
-    //switch for correct and incorrect guesses
-    this.correct = function(guess) {
-        if (guess) {
-            this.correctLetter = true;
-        } else {
-            this.correctLetter = false;
         }
     }
 }
@@ -44,10 +38,9 @@ function wordGuess() {
             message: "Guess a letter",
             name: "letter",
             validate: function(value) {
-                //if value is a number and value length is more than one
+                //if value is a number and value length is more than two
                 //does not work for values that aren't numbers and letters
                 if (!isNaN(value) || value.length > 2) {
-                    //console.log(!isNaN(value));
                     return false;
                 } else {
                     return true;
@@ -55,26 +48,27 @@ function wordGuess() {
             }    
         }
     ]).then(function(response) {
+        found = false;
         for (var i = 0; i < randomWord.length; i++) {
-            newLetter = new Letter(randomWord[i]);
+            var newLetter = new Letter(randomWord[i]);
             if (response.letter === randomWord[i]) {
-                newLetter.correct(true);
+                newLetter.correct = true;
+                found = true;
             } else {
-                newLetter.correct(false);
+                newLetter.correct = false;
             }
             newWord += newLetter.wordDisplay() + " ";
         }
-        //if letter guessed is the same as a letter in the word
-        //reveal the underscore with the letter
-        //console log correct
-        //else if not correct
-        //display the non-updated word
-        //console.log incorrect
         console.log(newWord);
-        wordGuess();
+        if (found === false) {
+            guess--;
+        }
+        console.log(`Guesses Left: ${guess}`);
+        //wordGuess();
     })
 }
 wordGuess();
 
 //notes
 //display guesses left
+//loop is currently creating a new word each time, so need to prevent that
