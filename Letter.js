d6e77aa347f9e66a9cade989f306b6e7ba79799c
elaintran <elain.ttran@gmail.com>
2019-06-25 19:59:17 -0500

    added guess checker

--- Letter.js
+++ Letter.js
@@ -1,7 +1,13 @@
+var inquirer = require("inquirer");
+
+//array of words
 var words = ["wordone", "wordtwo", "wordthree"];
+//getting a random index
 var index = Math.floor(Math.random() * words.length);
+//declaring a new word
 var randomWord = words[index];
-var potato;
+//start with an empty string
+//will print out undefined if left an empty variable
 var newWord = "";
 
 //function called every time the letters from the chosen word are looped through
@@ -14,18 +20,45 @@ function Letter(letter) {
     this.wordDisplay = function() {
         //if guessed correctly, reveal letter
         if (this.correctLetter === true) {
-            //console.log(this.letter);
             return this.letter;
         //if incorrect, display an underscore instead
         } else {
-            //console.log("_");
             return "_";
+        }
+    }
+    this.guess = function(guess) {
+        if (guess) {
+            this.correctLetter = true;
+        } else {
+            this.correctLetter = false;
         }
     }
 }
 
-for (var i = 0; i < randomWord.length; i++) {
-    newLetter = new Letter(randomWord[i]);
-    newWord += newLetter.wordDisplay() + " ";
-}
-console.log(newWord);
+//prompt users to make a guess
+inquirer.prompt([
+    {
+        type: "input",
+        message: "Guess a letter",
+        name: "letter"    
+    }
+]).then(function(response) {
+    for (var i = 0; i < randomWord.length; i++) {
+        if (response.letter === randomWord[i]) {
+            newLetter = new Letter(randomWord[i]);
+            newLetter.guess(true);
+        }
+        newWord += newLetter.wordDisplay() + " ";
+        newLetter.guess(false);
+    }
+    //if letter guessed is the same as a letter in the word
+    //reveal the underscore with the letter
+    //console log correct
+    //else if not correct
+    //display the non-updated word
+    //console.log incorrect
+    console.log(newWord);
+})
+
+//notes
+//display guesses left
--- index.js
+++ index.js
@@ -12,28 +12,3 @@ var inquirer = require("inquirer");
 // //console.log(hiddenWord[0] = "a");
 
 // // console.log(randomWord.split(""));
-
-// inquirer.prompt([
-//     {
-//         type: "input",
-//         message: "Guess a letter",
-//         name: "letter"    
-//     }
-// ]).then(function(response) {
-//     console.log(hiddenWord)
-//     for (var i = 0; i < randomWord.length; i++) {
-//         if (response.letter === randomWord[i]) {
-//             //console.log(hiddenWord[i] = randomWord[i]);
-//             //hiddenWord[i] = "a";
-//             //console.log(response.letter);
-//             //console.log(randomWord[i]);
-//             hiddenWord.charAt([i]).replace("_", randomWord[i]);
-//         }
-//     }
-//     //if letter guessed is the same as a letter in the word
-//     //reveal the underscore with the letter
-//     //console log correct
-//     //else if not correct
-//     //display the non-updated word
-//     //console.log incorrect
-// })
