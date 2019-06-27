a809d16d5f3e879c599d40710af0cd1ba480f94f
elaintran <elain.ttran@gmail.com>
2019-06-27 12:45:47 -0500

    created letter checker in word.js

--- Word.js
+++ Word.js
@@ -2,24 +2,30 @@ var Letter = require("./Letter.js");
 
 var Word = function(word) {
     this.word = word;
-    var letterArray = [];
     //var newLetters;
     var wordDisplay = "";
     this.array = function() {
+        var letterArray = [];
         for (var i = 0; i < this.word.length; i++) {
             var newLetters = new Letter(this.word[i]);
             letterArray.push(newLetters.letter);
         }
-        console.log(newLetters);
         console.log(letterArray);
     }
     this.display = function() {
         for (var i = 0; i < this.word.length; i++) {
             var newLetters = new Letter(this.word[i]);
-            newLetters.correct = false;
+            //set to false to display underscores
             wordDisplay += newLetters.wordDisplay() + " ";
         }
         console.log(wordDisplay);
+    }
+    this.checker = function(boolean) {
+        if (boolean) {
+            newLetters.correct = true;
+        } else {
+            newLetters.correct = false;
+        }
     }
 }
 
