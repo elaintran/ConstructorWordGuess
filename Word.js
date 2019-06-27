e23d84c3ba359e530d45db2c4958f6686c602ca4
elaintran <elain.ttran@gmail.com>
2019-06-27 12:41:47 -0500

    moved logic to index and created word constructor functions

--- Word.js
+++ Word.js
@@ -1,72 +1,30 @@
 var Letter = require("./Letter.js");
-var inquirer = require("inquirer");
 
-//array of words
-var words = ["wordone", "wordtwo", "wordthree"];
-//getting a random index
-var index = Math.floor(Math.random() * words.length);
-//declaring a new word
-var randomWord = words[index];
-//start with an empty string
-//will print out undefined if left an empty variable
-var newWord = "";
-//total guesses
-var guess = 10;
-//letter found default variable
-var found;
-
-//prompt users to make a guess
-function wordGuess() {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Guess a letter",
-            name: "letter",
-            validate: function(value) {
-                //if value is a number, value length is more than two, or there is no value
-                //does not work for values that aren't numbers and letters
-                //use regex to check for letters instead?
-                if (!isNaN(value) || value.length > 2 || value === "") {
-                    return false;
-                } else {
-                    return true;
-                }
-            }    
+var Word = function(word) {
+    this.word = word;
+    var letterArray = [];
+    //var newLetters;
+    var wordDisplay = "";
+    this.array = function() {
+        for (var i = 0; i < this.word.length; i++) {
+            var newLetters = new Letter(this.word[i]);
+            letterArray.push(newLetters.letter);
         }
-    ]).then(function(response) {
-        //found reset to false
-        found = false;
-        //loop through the word to check if letter is correct
-        for (var i = 0; i < randomWord.length; i++) {
-            //creates new letter function
-            var newLetter = new Letter(randomWord[i]);
-            //if letter is correct
-            if (response.letter === randomWord[i]) {
-                //add correct property as true into object
-                //returns letter
-                newLetter.correct = true;
-                //toggle found to true so it doesn't subtract the guesses
-                found = true;
-            } else {
-                //add correct property as false into object
-                //returns underscore
-                newLetter.correct = false;
-            }
-            //piece together the word with a space in between
-            newWord += newLetter.wordDisplay() + " ";
+        console.log(newLetters);
+        console.log(letterArray);
+    }
+    this.display = function() {
+        for (var i = 0; i < this.word.length; i++) {
+            var newLetters = new Letter(this.word[i]);
+            newLetters.correct = false;
+            wordDisplay += newLetters.wordDisplay() + " ";
         }
-        //display the new word on the console
-        console.log(newWord);
-        //if letter is not found, subtract guesses by one
-        if (found === false) {
-            guess--;
-        }
-        //display the remaining guesses
-        console.log(`Guesses Left: ${guess}`);
-        //wordGuess();
-    })
+        console.log(wordDisplay);
+    }
 }
-wordGuess();
 
-//notes
-//loop is currently creating a new word each time, so need to prevent that
+var newWord = new Word("testing");
+newWord.array();
+newWord.display();
+
+module.exports = Word;
--- index.js
+++ index.js
@@ -1,14 +1,73 @@
 var word = require("./word.js");
 var inquirer = require("inquirer");
 
+var Letter = require("./Letter.js");
 // var inquirer = require("inquirer");
 
-// function rand() {
-//     for (var i = 0; i < randomWord.length; i++) {
-//         hiddenWord += "_";
-//     }
-// }
-// rand();
-// //console.log(hiddenWord[0] = "a");
+// //array of words
+// var words = ["wordone", "wordtwo", "wordthree"];
+// //getting a random index
+// var index = Math.floor(Math.random() * words.length);
+// //declaring a new word
+// var randomWord = words[index];
+// //start with an empty string
+// //will print out undefined if left an empty variable
+// var newWord = "";
+// //total guesses
+// var guess = 10;
+// //letter found default variable
+// var found;
 
-// // console.log(randomWord.split(""));
+// //prompt users to make a guess
+// function wordGuess() {
+//     inquirer.prompt([
+//         {
+//             type: "input",
+//             message: "Guess a letter",
+//             name: "letter",
+//             validate: function(value) {
+//                 //if value is a number, value length is more than two, or there is no value
+//                 //does not work for values that aren't numbers and letters
+//                 //use regex to check for letters instead?
+//                 //or loop through an alphabet array
+//                 if (!isNaN(value) || value.length > 2 || value === "") {
+//                     return false;
+//                 } else {
+//                     return true;
+//                 }
+//             }    
+//         }
+//     ]).then(function(response) {
+//         //found reset to false
+//         found = false;
+//         //loop through the word to check if letter is correct
+//         for (var i = 0; i < randomWord.length; i++) {
+//             //creates new letter function
+//             var newLetter = new Letter(randomWord[i]);
+//             //if letter is correct
+//             if (response.letter === randomWord[i]) {
+//                 //add correct property as true into object
+//                 //returns letter
+//                 newLetter.correct = true;
+//                 //toggle found to true so it doesn't subtract the guesses
+//                 found = true;
+//             } else {
+//                 //add correct property as false into object
+//                 //returns underscore
+//                 newLetter.correct = false;
+//             }
+//             //piece together the word with a space in between
+//             newWord += newLetter.wordDisplay() + " ";
+//         }
+//         //display the new word on the console
+//         console.log(newWord);
+//         //if letter is not found, subtract guesses by one
+//         if (found === false) {
+//             guess--;
+//         }
+//         //display the remaining guesses
+//         console.log(`Guesses Left: ${guess}`);
+//         //wordGuess();
+//     })
+// }
+// wordGuess();
